02c9b375e481fa17b218aab55827357270823f21
s1822284 <t.g.sierink@student.utwente.nl>
2022-11-07 15:02:18 +0100

    day 6

--- day6.py
+++ day6.py
@@ -1,5 +1,31 @@
-from ast import main
+DAYS = 256
 
-
-file = open("input/day6input.txt", "r").read().split(',')
-
+if __name__ == __name__:
+    file = open("input/day6input.txt", "r").read().split(',')
+    state = [int(f) for f in file]
+    
+    # print(state)
+    # state = [3,4,3,1,2]
+    
+    state_dict = {}
+    for i in range(9):
+        state_dict[f"{i}"] = state.count(i)
+    
+    print(state_dict)
+    
+    for i in range(DAYS):
+        print(i)
+        temp_0 = state_dict.get("0")
+        
+        state_dict["0"] = state_dict.get("1")
+        state_dict["1"] = state_dict.get("2")
+        state_dict["2"] = state_dict.get("3")
+        state_dict["3"] = state_dict.get("4")
+        state_dict["4"] = state_dict.get("5")
+        state_dict["5"] = state_dict.get("6")
+        state_dict["6"] = temp_0 +state_dict.get("7")
+        state_dict["7"] = state_dict.get("8")
+        state_dict["8"] = temp_0
+               
+        
+    print(sum(state_dict.values()))   
